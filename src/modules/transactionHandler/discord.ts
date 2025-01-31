b7007dea2f4ab8f2ef735ee2e77859914cbb430e
billos <billos@billos.fr>
2025-01-31 16:41:15 +0100

    refactor(transactionHandler): optimize Discord message handling by reusing Axios instance

--- src/modules/transactionHandler/discord.ts
+++ src/modules/transactionHandler/discord.ts
@@ -1,27 +1,25 @@
-import axios from "axios"
+import axios, { AxiosInstance } from "axios"
 
 import { env } from "../../config"
 import { AbstractTransactionHandler } from "./transactionHandler"
 
 export class DiscordTransactionHandler extends AbstractTransactionHandler {
+  private request: AxiosInstance = axios.create({})
+
   constructor() {
     super()
   }
 
   override async sendMessageImpl(content: string): Promise<string> {
-    const botInstance = axios.create({})
-    const result = await botInstance.post<{ id: number }>(`${env.discordWebhook}?wait=true`, { content })
+    const result = await this.request.post<{ id: number }>(`${env.discordWebhook}?wait=true`, { content })
     return `${result.data.id}`
   }
 
   override async updateMessageImpl(id: string, content: string): Promise<void> {
-    const botInstance = axios.create({})
-    await botInstance.patch(`${env.discordWebhook}/messages/${id}`, { content })
+    await this.request.patch(`${env.discordWebhook}/messages/${id}`, { content })
   }
 
   override async deleteMessageImpl(id: string): Promise<void> {
-    const botInstance = axios.create({})
-    console.log("Deleting message url is", `${env.discordWebhook}/messages/${id}`)
-    await botInstance.delete(`${env.discordWebhook}/messages/${id}`)
+    await this.request.delete(`${env.discordWebhook}/messages/${id}`)
   }
 }
