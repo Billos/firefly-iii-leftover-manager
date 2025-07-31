d70cffbe070582e10f581ecccd30bacb6e24c7ca
billos <billos@billos.fr>
2025-08-01 00:44:18 +0200

    refactor(date): correct timezone handling in getDateNow function

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -3,11 +3,11 @@ import { DateTime } from "luxon"
 export function getDateNow(): DateTime {
   // Returns the current date in ISO format without time
   const date = DateTime.now()
-
   if (process.env.TZ) {
     // If a timezone is set, return the date in that timezone
-    return date.setZone(process.env.TZ)
+    date.setZone(process.env.TZ)
   }
 
+  console.log("Current time is ", date.toISO())
   return date
 }
